5129a9e00dfcbb563d8a53b758d4bbbea9a34447
nobody <nobody@b0f73b58-f77c-4d9d-b241-b801c8a9558b>
2009-08-12 17:37:08 +0000

    Tagging

---
+++ tags/0.25/src/enemy/enemies.c
@@ -0,0 +1,138 @@
+/*
+Copyright (C) 2009 Parallel Realities
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include "../headers.h"
+
+#include "../graphics/animation.h"
+#include "../system/properties.h"
+#include "../entity.h"
+#include "../system/random.h"
+#include "../audio/audio.h"
+
+#include "bat.h"
+#include "chicken.h"
+#include "rock.h"
+#include "spider.h"
+#include "grub.h"
+#include "fireball.h"
+#include "wasp.h"
+#include "small_boulder.h"
+#include "red_grub.h"
+#include "spitting_plant.h"
+#include "jumping_slime.h"
+#include "egg.h"
+#include "baby_slime.h"
+#include "spinner.h"
+#include "floating_snapper.h"
+#include "enemy_generator.h"
+#include "flying_bug.h"
+#include "eye_stalk.h"
+#include "snail.h"
+#include "snail_shell.h"
+#include "scorpion.h"
+#include "laser_grid.h"
+#include "ceiling_snapper.h"
+#include "gazer.h"
+#include "sludge.h"
+
+#include "../boss/ant_lion.h"
+#include "../boss/boulder_boss.h"
+#include "../boss/grub_boss.h"
+#include "../boss/golem_boss.h"
+#include "../boss/snake_boss.h"
+#include "../boss/fly_boss.h"
+#include "../boss/flying_maggot.h"
+#include "../boss/golem_rock_dropper.h"
+
+static Constructor enemies[] = {
+{"enemy/bat", &addBat},
+{"enemy/red_bat", &addBat},
+{"enemy/chicken", &addChicken},
+{"common/large_rock", &addLargeRock},
+{"boss/boulder_boss", &addBoulderBoss},
+{"enemy/spider", &addSpider},
+{"enemy/red_spider", &addSpider},
+{"enemy/grub", &addGrub},
+{"boss/grub_boss", &addGrubBoss},
+{"enemy/jumping_fireball", &addJumpingFireball},
+{"enemy/wasp", &addWasp},
+{"common/small_rock", &addSmallRock},
+{"enemy/small_boulder", &addSmallBoulder},
+{"enemy/red_grub", &addRedGrub},
+{"enemy/spitting_plant", &addSpittingPlant},
+{"enemy/jumping_slime", &addJumpingSlime},
+{"enemy/purple_jumping_slime", &addJumpingSlime},
+{"enemy/jumping_slime_egg", &addEgg},
+{"boss/golem_boss", &addGolemBoss},
+{"enemy/baby_slime", &addBabySlime},
+{"enemy/red_baby_slime", &addBabySlime},
+{"enemy/eye_spinner", &addSpinner},
+{"boss/snake_boss", &addSnakeBoss},
+{"enemy/floating_snapper", &addFloatingSnapper},
+{"enemy/bee_hive", &addEnemyGenerator},
+{"enemy/flying_bug", &addFlyingBug},
+{"boss/fly_boss", &addFlyBoss},
+{"boss/ant_lion", &addAntLion},
+{"enemy/eye_stalk", &addEyeStalk},
+{"enemy/snail", &addSnail},
+{"enemy/purple_snail", &addSnail},
+{"enemy/purple_snail_shell", &addSnailShell},
+{"enemy/snail_shell", &addSnailShell},
+{"boss/flying_maggot", &addFlyingMaggot},
+{"boss/flying_maggot_2", &addFlyingMaggot},
+{"enemy/scorpion", &addScorpion},
+{"enemy/laser_grid", &addLaserGrid},
+{"enemy/ceiling_snapper", &addCeilingSnapper},
+{"enemy/gazer", &addGazer},
+{"enemy/sludge", &addSludge},
+{"boss/golem_rock_dropper", &addGolemRockDropper},
+{"enemy/tutorial_spitting_plant", &addSpittingPlant}
+};
+
+static int length = sizeof(enemies) / sizeof(Constructor);
+
+Entity *addEnemy(char *name, int x, int y)
+{
+	int i;
+	Entity *e;
+
+	for (i=0;i<length;i++)
+	{
+		if (strcmpignorecase(enemies[i].name, name) == 0)
+		{
+			e = enemies[i].construct(x, y, name);
+
+			if (e->fallout == NULL)
+			{
+				e->fallout = &entityDie;
+			}
+
+			if (e->pain == NULL)
+			{
+				e->pain = &enemyPain;
+			}
+
+			return e;
+		}
+	}
+
+	printf("Could not find enemy %s\n", name);
+
+	exit(1);
+}
---
+++ tags/0.25/src/item/grabber.c
@@ -0,0 +1,336 @@
+/*
+Copyright (C) 2009 Parallel Realities
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include "../headers.h"
+
+#include "../graphics/animation.h"
+#include "../audio/audio.h"
+#include "../system/properties.h"
+#include "../entity.h"
+#include "../collisions.h"
+#include "../geometry.h"
+#include "../event/script.h"
+
+extern Entity *self, entity[MAX_ENTITIES];
+
+static void wait(void);
+static void dropRock(void);
+static void grabRock(void);
+static void moveToTarget(void);
+static void moveToStart(void);
+static void moveToEnd(void);
+static void touch(Entity *);
+static void activate(int);
+static void addChain(Entity *);
+static void chainWait(void);
+
+Entity *addGrabber(int x, int y, char *name)
+{
+	Entity *e = getFreeEntity();
+
+	if (e == NULL)
+	{
+		printf("No free slots to add Grabber\n");
+
+		exit(1);
+	}
+
+	loadProperties(name, e);
+
+	e->x = x;
+	e->y = y;
+
+	e->type = KEY_ITEM;
+
+	e->face = LEFT;
+
+	e->action = &wait;
+	e->touch = &touch;
+	e->activate = &activate;
+
+	e->draw = &drawLoopingAnimationToMap;
+
+	setEntityAnimation(e, STAND);
+
+	addChain(e);
+
+	return e;
+}
+
+static void wait()
+{
+	if (self->health != 1 && self->active == TRUE)
+	{
+		if (self->target == NULL)
+		{
+			setEntityAnimation(self, WALK);
+
+			self->action = &grabRock;
+		}
+
+		else
+		{
+			self->thinkTime = 60;
+
+			setEntityAnimation(self, WALK);
+
+			self->action = &dropRock;
+		}
+	}
+
+	else if (self->health == 1)
+	{
+		self->action = &moveToEnd;
+	}
+
+	checkToMap(self);
+}
+
+static void dropRock()
+{
+	if (self->target != NULL)
+	{
+		self->target->flags &= ~(HELPLESS|FLY);
+
+		self->target->type = PROJECTILE;
+
+		self->target->damage = 100;
+
+		self->target->parent = self;
+
+		self->target = NULL;
+	}
+
+	else
+	{
+		self->thinkTime--;
+
+		if (self->thinkTime <= 0)
+		{
+			setEntityAnimation(self, STAND);
+			
+			moveToStart();
+		}
+	}
+}
+
+static void grabRock()
+{
+	self->dirY = self->speed;
+
+	checkToMap(self);
+
+	if (self->flags & ON_GROUND)
+	{
+		setEntityAnimation(self, STAND);
+
+		moveToStart();
+	}
+}
+
+static void moveToTarget()
+{
+	checkToMap(self);
+
+	if (self->target != NULL)
+	{
+		self->target->x += self->dirX;
+		self->target->y += self->dirY;
+	}
+
+	if (fabs(self->x - self->targetX) <= fabs(self->dirX) && fabs(self->y - self->targetY) <= fabs(self->dirY))
+	{
+		self->x = self->targetX;
+		self->y = self->targetY;
+
+		self->dirX = 0;
+		self->dirY = 0;
+
+		if (self->health == 1)
+		{
+			if (self->x == self->endX && self->y == self->endY)
+			{
+				setEntityAnimation(self, STAND);
+				
+				self->health = 2;
+	
+				self->startX = self->endX;
+				self->startY = self->endY;
+			}
+		}
+
+		self->active = FALSE;
+
+		self->action = &wait;
+	}
+}
+
+static void touch(Entity *other)
+{
+	Entity *temp;
+	
+	if (self->active == TRUE)
+	{
+		if ((other->flags & ON_GROUND) && strcmpignorecase(other->name, "enemy/small_boulder") == 0)
+		{
+			setEntityAnimation(self, STAND);
+
+			other->flags |= HELPLESS|FLY;
+
+			other->touch = &entityTouch;
+
+			other->damage = 0;
+
+			self->target = other;
+
+			self->action = &moveToEnd;
+
+			self->active = FALSE;
+
+			self->dirY = -self->speed;
+
+			self->dirX = 1;
+		}
+
+		else if (strcmpignorecase(other->name, "boss/golem_boss") == 0)
+		{
+			setEntityAnimation(self, STAND);
+
+			if (other->currentAnim == other->animation[CUSTOM_1] && self->health == 2)
+			{
+				temp = self;
+				
+				self = other;
+				
+				self->die();
+				
+				self = temp;
+			}
+
+			else
+			{
+				moveToStart();
+			}
+		}
+	}
+}
+
+static void moveToStart()
+{
+	self->targetX = self->startX;
+	self->targetY = self->startY;
+
+	calculatePath(self->x, self->y, self->startX, self->startY, &self->dirX, &self->dirY);
+
+	self->dirX *= self->speed;
+	self->dirY *= self->speed;
+
+	self->action = &moveToTarget;
+
+	self->active = FALSE;
+}
+
+static void moveToEnd()
+{
+	self->targetX = self->endX;
+	self->targetY = self->endY;
+
+	calculatePath(self->x, self->y, self->endX, self->endY, &self->dirX, &self->dirY);
+
+	self->dirX *= self->speed;
+	self->dirY *= self->speed;
+
+	self->action = &moveToTarget;
+
+	self->active = FALSE;
+}
+
+static void activate(int val)
+{
+	self->active = TRUE;
+}
+
+static void addChain(Entity *parent)
+{
+	int chainHeight, i, chainCount, y;
+	Entity *e, *previous;
+
+	e = getFreeEntity();
+
+	if (e == NULL)
+	{
+		printf("No free slots to add Grabber Chain\n");
+
+		exit(1);
+	}
+
+	loadProperties("item/grabber_chain", e);
+
+	chainHeight = e->h;
+
+	chainCount = (SCREEN_HEIGHT / chainHeight) + 1;
+
+	e->inUse = FALSE;
+
+	y = parent->y;
+
+	previous = parent;
+
+	for (i=0;i<chainCount;i++)
+	{
+		e = getFreeEntity();
+
+		if (e == NULL)
+		{
+			printf("No free slots to add Grabber Chain\n");
+
+			exit(1);
+		}
+
+		loadProperties("item/grabber_chain", e);
+
+		e->x = parent->x + parent->w / 2;
+
+		e->x -= e->w / 2;
+
+		e->y = y - e->h;
+
+		e->action = &chainWait;
+		e->touch = NULL;
+
+		e->head = previous;
+
+		e->draw = &drawLoopingAnimationToMap;
+
+		setEntityAnimation(e, STAND);
+
+		y -= chainHeight;
+
+		previous = e;
+	}
+}
+
+static void chainWait()
+{
+	self->x = self->head->x + self->head->w / 2;
+	self->x -= self->w / 2;
+
+	self->y = self->head->y;
+	self->y -= self->h;
+}
