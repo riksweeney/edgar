c2ced1ce266aca1819b6af3d3972b497490af08d
nobody <nobody@b0f73b58-f77c-4d9d-b241-b801c8a9558b>
2010-05-17 15:37:25 +0000

    Tagging 0.56

---
+++ tags/0.56/map_diff.sh
@@ -0,0 +1,2 @@
+svn diff http://reddwarf/svn/Edgar/tags/$1/data/maps http://reddwarf/svn/Edgar/trunk/data/maps >map_diff.txt
+svn diff http://reddwarf/svn/Edgar/tags/$1/src http://reddwarf/svn/Edgar/trunk/src >src_diff.txt
