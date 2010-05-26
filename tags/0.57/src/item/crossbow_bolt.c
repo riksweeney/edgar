903b9288ff842c0e101c6a99daa47de9002d483d
nobody <nobody@b0f73b58-f77c-4d9d-b241-b801c8a9558b>
2010-05-26 16:45:24 +0000

    Tagging 0.57

---
+++ tags/0.57/src/item/crossbow_bolt.c
@@ -0,0 +1,137 @@
+/*
+Copyright (C) 2009-2010 Parallel Realities
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include "../headers.h"
+
+#include "../graphics/animation.h"
+#include "../system/properties.h"
+#include "../entity.h"
+#include "../game.h"
+#include "../audio/audio.h"
+#include "../collisions.h"
+#include "../system/error.h"
+#include "../system/random.h"
+#include "../enemy/rock.h"
+
+extern Entity *self;
+
+static void wait(void);
+static void stickInWall(void);
+static void init(void);
+
+Entity *addCrossbowBolt(int x, int y, char *name)
+{
+	Entity *e = getFreeEntity();
+
+	if (e == NULL)
+	{
+		showErrorAndExit("No free slots to add a Crossbow Bolt");
+	}
+
+	loadProperties(name, e);
+
+	e->x = x;
+	e->y = y;
+
+	e->type = KEY_ITEM;
+
+	e->face = RIGHT;
+
+	e->action = &init;
+
+	e->draw = &drawLoopingAnimationToMap;
+
+	setEntityAnimation(e, STAND);
+
+	e->thinkTime = 0;
+
+	return e;
+}
+
+static void init()
+{
+	self->action = self->mental == 1 ? &stickInWall : &wait;
+
+	self->touch = self->mental == 1 ? &pushEntity : NULL;
+}
+
+static void wait()
+{
+	int i;
+	float dirX;
+	Entity *e;
+	
+	dirX = self->dirX;
+	
+	if (dirX != 0)
+	{
+		for (e=self->target;e!=NULL;e=e->target)
+		{
+			if (e->x > self->startX)
+			{
+				e->flags &= ~NO_DRAW;
+			}
+			
+			else
+			{
+				e->flags |= NO_DRAW;
+			}
+		}
+	}
+	
+	checkToMap(self);
+	
+	if (self->dirX == 0 && dirX != 0)
+	{
+		playSoundToMap("sound/common/crumble.ogg", -1, self->x, self->y, 0);
+		
+		self->mental = 1;
+		
+		self->touch = &pushEntity;
+		
+		self->x += TILE_SIZE / 2;
+		
+		for (e=self->target;e!=NULL;e=e->target)
+		{
+			e->x += TILE_SIZE / 2;
+			
+			e->dirX = 0;
+			
+			e->touch = &pushEntity;
+		}
+		
+		for (i=0;i<6;i++)
+		{
+			e = addSmallRock(self->x, self->y, "common/small_rock");
+
+			e->x += (self->w - e->w) / 2;
+			e->y += (self->h - e->h) / 2;
+
+			e->dirX = -(prand() % 4 + 1);
+			e->dirY = -(prand() % 3 + 3);
+		}
+		
+		shakeScreen(MEDIUM, 15);
+	}
+}
+
+static void stickInWall()
+{
+	self->dirY = 0;
+}
---
+++ tags/0.57/src/item/light_target.c
@@ -0,0 +1,139 @@
+/*
+Copyright (C) 2009-2010 Parallel Realities
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include "../headers.h"
+
+#include "../audio/audio.h"
+#include "../graphics/animation.h"
+#include "../system/properties.h"
+#include "../entity.h"
+#include "../collisions.h"
+#include "../hud.h"
+#include "../system/error.h"
+
+extern Entity *self;
+extern Game game;
+
+static void init(void);
+static void wait(void);
+static void activate(int);
+static void touch(Entity *);
+
+Entity *addLightTarget(int x, int y, char *name)
+{
+	Entity *e = getFreeEntity();
+
+	if (e == NULL)
+	{
+		showErrorAndExit("No free slots to add a Light Target");
+	}
+
+	loadProperties(name, e);
+
+	e->touch = &touch;
+
+	e->action = &init;
+
+	e->draw = &drawLoopingAnimationToMap;
+
+	e->activate = &activate;
+
+	e->x = x;
+	e->y = y;
+
+	setEntityAnimation(e, STAND);
+
+	return e;
+}
+
+static void init()
+{
+	self->thinkTime = -1;
+
+	self->action = &wait;
+}
+
+static void wait()
+{
+	self->thinkTime--;
+
+	if (self->thinkTime == 0)
+	{
+		setEntityAnimation(self, STAND);
+
+		activateEntitiesWithRequiredName(self->objectiveName, FALSE);
+
+		self->active = FALSE;
+		
+		self->thinkTime = -1;
+	}
+
+	else if (self->thinkTime < 0)
+	{
+		self->thinkTime = -1;
+	}
+}
+
+static void touch(Entity *other)
+{	
+	if (strcmpignorecase(other->name, "item/light_beam") == 0)
+	{
+		activate(1);
+	}
+
+	else
+	{
+		pushEntity(other);
+	}
+}
+
+static void activate(int val)
+{
+	int remaining, total;
+
+	if (self->thinkTime == -1)
+	{
+		setEntityAnimation(self, WALK);
+
+		self->active = TRUE;
+
+		remaining = countSiblings(self, &total);
+
+		if (remaining == 0)
+		{
+			activateEntitiesWithRequiredName(self->objectiveName, TRUE);
+
+			if (total > 0)
+			{
+				setInfoBoxMessage(60, 255, 255, 255, _("Complete"));
+			}
+		}
+
+		else
+		{
+			setInfoBoxMessage(30, 255, 255, 255, _("%d more to go..."), remaining);
+		}
+
+		self->maxHealth = self->health;
+
+		playSoundToMap("sound/common/switch.ogg", -1, self->x, self->y, 0);
+	}
+
+	self->thinkTime = 5;
+}
