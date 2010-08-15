485310884bc737de02f37d7cd61a326fb3f32859
nobody <nobody@b0f73b58-f77c-4d9d-b241-b801c8a9558b>
2010-08-15 14:53:58 +0000

    Tagging 0.67

---
+++ tags/0.67/src/enemy/centurion.c
@@ -0,0 +1,646 @@
+/*
+Copyright (C) 2009-2010 Parallel Realities
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include "../headers.h"
+
+#include "../graphics/animation.h"
+#include "../graphics/decoration.h"
+#include "../system/properties.h"
+#include "../entity.h"
+#include "../system/random.h"
+#include "../audio/audio.h"
+#include "../collisions.h"
+#include "../custom_actions.h"
+#include "../game.h"
+#include "../player.h"
+#include "../geometry.h"
+#include "../item/item.h"
+#include "../event/trigger.h"
+#include "../event/global_trigger.h"
+#include "../system/error.h"
+#include "../world/explosion.h"
+
+extern Entity *self, player;
+
+static void lookForPlayer(void);
+static void stompAttackInit(void);
+static void stompAttack(void);
+static void stompAttackFinish(void);
+static void attacking(void);
+static void die(void);
+static void redDie(void);
+static void reform(void);
+static void dieWait(void);
+static void pieceWait(void);
+static void pieceReform(void);
+static void pieceFallout(void);
+static void reformFinish(void);
+static void explosionAttackInit(void);
+static void explosionAttack(void);
+static void explosionAttackFinish(void);
+static void explosionMove(void);
+static void explode(void);
+static void explosionTouch(Entity *);
+static void redTakeDamage(Entity *, int);
+
+Entity *addCenturion(int x, int y, char *name)
+{
+	Entity *e = getFreeEntity();
+
+	if (e == NULL)
+	{
+		showErrorAndExit("No free slots to add a Centurion");
+	}
+
+	loadProperties(name, e);
+
+	e->x = x;
+	e->y = y;
+
+	e->action = &lookForPlayer;
+	e->die = strcmpignorecase(name, "enemy/red_centurion") == 0 ? &redDie : &die;
+	e->draw = &drawLoopingAnimationToMap;
+	e->touch = &entityTouch;
+	e->takeDamage = strcmpignorecase(name, "enemy/red_centurion") == 0 ? &redTakeDamage : &entityTakeDamageNoFlinch;
+	e->reactToBlock = &changeDirection;
+
+	e->type = ENEMY;
+
+	setEntityAnimation(e, STAND);
+
+	return e;
+}
+
+static void lookForPlayer()
+{
+	if (self->maxThinkTime == 1)
+	{
+		if (!(self->flags & ATTRACTED))
+		{
+			self->dirX = (self->face == RIGHT ? self->speed : -self->speed);
+		}
+	}
+	
+	if (self->currentFrame == 2 || self->currentFrame == 5)
+	{
+		if (self->maxThinkTime == 0)
+		{
+			if (self->flags & ON_GROUND)
+			{
+				playSoundToMap("sound/enemy/centurion/walk.ogg", -1, self->x, self->y, 0);
+			}
+
+			self->maxThinkTime = 1;
+		}
+		
+		if (!(self->flags & ATTRACTED))
+		{
+			self->dirX = self->standingOn == NULL ? 0 : self->standingOn->dirX;
+		}
+	}
+
+	else
+	{
+		self->maxThinkTime = 0;
+	}
+
+	if (self->maxThinkTime == 0 && (self->dirX == 0 || isAtEdge(self) == TRUE))
+	{
+		self->dirX = (self->face == RIGHT ? -self->speed : self->speed);
+
+		self->face = (self->face == RIGHT ? LEFT : RIGHT);
+	}
+	
+	checkToMap(self);
+
+	self->thinkTime--;
+
+	if (self->thinkTime <= 0)
+	{
+		self->thinkTime = 0;
+	}
+	
+	if (strcmpignorecase(self->name, "enemy/red_centurion") == 0)
+	{
+		if (self->thinkTime == 0 && player.health > 0 && prand() % 10 == 0)
+		{
+			if (collision(self->x + (self->face == RIGHT ? self->w : -240), self->y, 240, self->h, player.x, player.y, player.w, player.h) == 1)
+			{
+				self->action = &explosionAttackInit;
+				
+				if (!(self->flags & ATTRACTED))
+				{
+					self->dirX = 0;
+				}
+			}
+		}
+	}
+	
+	else
+	{
+		if (self->thinkTime == 0 && player.health > 0 && prand() % 15 == 0)
+		{
+			if (collision(self->x + (self->face == RIGHT ? self->w : -160), self->y, 160, self->h, player.x, player.y, player.w, player.h) == 1)
+			{
+				self->action = &stompAttackInit;
+				
+				if (!(self->flags & ATTRACTED))
+				{
+					self->dirX = 0;
+				}
+			}
+		}
+	}
+}
+
+static void stompAttackInit()
+{
+	setEntityAnimation(self, ATTACK_1);
+
+	self->animationCallback = &stompAttack;
+
+	self->action = &attacking;
+
+	checkToMap(self);
+}
+
+static void stompAttack()
+{
+	setEntityAnimation(self, ATTACK_2);
+
+	playSoundToMap("sound/common/crash.ogg", -1, self->x, self->y, 0);
+
+	shakeScreen(MEDIUM, 60);
+
+	self->thinkTime = 60;
+
+	if (player.flags & ON_GROUND)
+	{
+		setPlayerStunned(120);
+	}
+
+	self->action = &stompAttackFinish;
+}
+
+static void stompAttackFinish()
+{
+	self->thinkTime--;
+
+	if (self->thinkTime <= 0)
+	{
+		setEntityAnimation(self, STAND);
+
+		self->action = &lookForPlayer;
+
+		self->thinkTime = 60;
+
+		self->dirX = (self->face == RIGHT ? self->speed : -self->speed);
+	}
+}
+
+static void explosionAttackInit()
+{
+	setEntityAnimation(self, ATTACK_1);
+
+	self->animationCallback = &explosionAttack;
+
+	self->action = &attacking;
+
+	checkToMap(self);
+}
+
+static void explosionAttack()
+{
+	Entity *e = getFreeEntity();
+	
+	if (e == NULL)
+	{
+		showErrorAndExit("No free slots to add a moving explosion");
+	}
+
+	loadProperties("common/explosion", e);
+	
+	setEntityAnimation(e, STAND);
+
+	e->x = self->x + self->w / 2 - e->w / 2;
+	e->y = self->y + self->h - e->h;
+	
+	e->face = self->face;
+	
+	e->dirX = e->face == LEFT ? -6 : 6;
+	
+	e->damage = 1;
+
+	e->action = &explosionMove;
+	e->draw = &drawLoopingAnimationToMap;
+	e->touch = &explosionTouch;
+
+	e->type = ENEMY;
+	
+	e->flags |= NO_DRAW|DO_NOT_PERSIST;
+	
+	e->startX = playSoundToMap("sound/boss/ant_lion/earthquake.ogg", -1, self->x, self->y, -1);
+	
+	setEntityAnimation(self, ATTACK_2);
+
+	self->thinkTime = 60;
+
+	self->action = &explosionAttackFinish;
+	
+	checkToMap(self);
+}
+
+static void explosionAttackFinish()
+{
+	self->thinkTime--;
+
+	if (self->thinkTime <= 0)
+	{
+		setEntityAnimation(self, STAND);
+
+		self->action = &lookForPlayer;
+
+		self->thinkTime = 60;
+
+		self->dirX = (self->face == RIGHT ? self->speed : -self->speed);
+	}
+	
+	checkToMap(self);
+}
+
+static void explosionMove()
+{
+	Entity *e;
+	
+	e = addSmoke(self->x + (prand() % self->w), self->y + self->h, "decoration/dust");
+
+	if (e != NULL)
+	{
+		e->y -= prand() % e->h;
+	}
+	
+	checkToMap(self);
+	
+	if (isAtEdge(self) == TRUE || self->dirX == 0)
+	{
+		self->touch = NULL;
+		
+		self->dirX = 0;
+		
+		self->mental = 5;
+		
+		self->action = &explode;
+	}
+}
+
+static void explosionTouch(Entity *other)
+{
+	entityTouch(other);
+	
+	if (other->type == PLAYER)
+	{
+		self->touch = NULL;
+		
+		self->dirX = 0;
+		
+		self->mental = 5;
+		
+		self->action = &explode;
+	}
+}
+
+static void explode()
+{
+	int x, y;
+	Entity *e;
+
+	self->thinkTime--;
+	
+	stopSound(self->startX);
+	
+	self->startX = -1;
+
+	if (self->thinkTime <= 0)
+	{
+		x = self->x + self->w / 2;
+		y = self->y + self->h / 2;
+
+		x += (prand() % 32) * (prand() % 2 == 0 ? 1 : -1);
+		y += (prand() % 32) * (prand() % 2 == 0 ? 1 : -1);
+
+		e = addExplosion(x, y);
+		
+		e->type = ENEMY;
+		
+		e->damage = 1;
+
+		self->mental--;
+
+		self->thinkTime = 10;
+
+		if (self->mental == 0)
+		{
+			self->inUse = FALSE;
+		}
+	}
+}
+
+static void attacking()
+{
+	checkToMap(self);
+}
+
+static void die()
+{
+	int i;
+	Entity *e;
+	char name[MAX_VALUE_LENGTH];
+
+	snprintf(name, sizeof(name), "%s_piece", self->name);
+
+	fireTrigger(self->objectiveName);
+
+	fireGlobalTrigger(self->objectiveName);
+
+	for (i=0;i<7;i++)
+	{
+		e = addTemporaryItem(name, self->x, self->y, self->face, 0, 0);
+
+		e->x += (self->w - e->w) / 2;
+		e->y += (self->w - e->w) / 2;
+
+		e->dirX = (prand() % 5) * (prand() % 2 == 0 ? -1 : 1);
+		e->dirY = ITEM_JUMP_HEIGHT + (prand() % ITEM_JUMP_HEIGHT);
+
+		if (i == 2 || i == 3)
+		{
+			setEntityAnimation(e, i == 2 ? 0 : 1);
+		}
+
+		else
+		{
+			setEntityAnimation(e, i);
+		}
+
+		e->thinkTime = 60 + (prand() % 180);
+	}
+
+	self->damage = 0;
+
+	if (!(self->flags & INVULNERABLE))
+	{
+		self->flags &= ~FLY;
+
+		self->flags |= (DO_NOT_PERSIST|NO_DRAW);
+
+		self->thinkTime = 60;
+
+		setCustomAction(self, &invulnerableNoFlash, 240, 0, 0);
+
+		self->frameSpeed = 0;
+
+		self->action = &standardDie;
+
+		self->damage = 0;
+	}
+}
+
+static void redDie()
+{
+	int i;
+	Entity *e;
+	char name[MAX_VALUE_LENGTH];
+
+	snprintf(name, sizeof(name), "%s_piece", self->name);
+
+	for (i=0;i<9;i++)
+	{
+		e = addTemporaryItem(name, self->x, self->y, self->face, 0, 0);
+		
+		e->action = &pieceWait;
+		
+		e->fallout = &pieceFallout;
+
+		e->x += (self->w - e->w) / 2;
+		e->y += (self->w - e->w) / 2;
+
+		e->dirX = (prand() % 5) * (prand() % 2 == 0 ? -1 : 1);
+		e->dirY = ITEM_JUMP_HEIGHT + (prand() % ITEM_JUMP_HEIGHT);
+
+		setEntityAnimation(e, i);
+		
+		e->head = self;
+	}
+	
+	self->endX = self->damage;
+	
+	self->damage = 0;
+	
+	self->health = 0;
+	
+	self->dirX = 0;
+
+	self->mental = 1;
+	
+	self->flags &= ~FLY;
+
+	self->flags |= NO_DRAW;
+	
+	self->takeDamage = NULL;
+
+	self->action = &dieWait;
+	
+	self->thinkTime = 120;
+}
+
+static void dieWait()
+{
+	self->thinkTime--;
+	
+	if (self->thinkTime <= 0)
+	{
+		self->mental = 0;
+		
+		self->thinkTime = 9;
+		
+		self->action = &reform;
+	}
+}
+
+static void reform()
+{
+	if (self->health == -1)
+	{
+		increaseKillCount();
+		
+		dropRandomItem(self->x + self->w / 2, self->y);
+		
+		self->action = &entityDieVanish;
+	}
+	
+	if (self->thinkTime == 0)
+	{
+		self->thinkTime = 30;
+		
+		self->action = &reformFinish;
+	}
+}
+
+static void reformFinish()
+{
+	self->thinkTime--;
+	
+	if (self->thinkTime <= 0)
+	{
+		self->health = self->maxHealth;
+		
+		self->flags &= ~NO_DRAW;
+		
+		self->action = &lookForPlayer;
+		
+		facePlayer();
+		
+		self->dirX = self->face == LEFT ? -self->speed : self->speed;
+		
+		self->damage = 1;
+		
+		self->takeDamage = &redTakeDamage;
+		
+		setEntityAnimation(self, STAND);
+	}
+}
+
+static void pieceWait()
+{
+	if (self->head->health == -1)
+	{
+		self->action = &entityDieNoDrop;
+	}
+	
+	else if (self->head->mental == 0)
+	{
+		self->action = &pieceReform;
+		
+		if (self->face == LEFT)
+		{
+			self->targetX = self->head->x + self->head->w - self->w - self->offsetX;
+		}
+		
+		else
+		{
+			self->targetX = self->head->x + self->offsetX;
+		}
+		
+		self->targetY = self->head->y + self->offsetY;
+		
+		calculatePath(self->x, self->y, self->targetX, self->targetY, &self->dirX, &self->dirY);
+
+		self->dirX *= 4;
+		self->dirY *= 4;
+		
+		self->flags |= FLY;
+		
+		self->touch = NULL;
+	}
+	
+	if ((self->flags & ON_GROUND) && !(self->flags & FLY))
+	{
+		self->dirX = 0;
+	}
+	
+	checkToMap(self);
+}
+
+static void pieceReform()
+{
+	if (atTarget())
+	{
+		if (self->mental == 0)
+		{
+			self->mental = 1;
+			
+			self->head->thinkTime--;
+		}
+		
+		else
+		{
+			if (self->head->health == self->head->maxHealth)
+			{
+				self->inUse = FALSE;
+			}
+		}
+	}
+	
+	else
+	{
+		self->x += self->dirX;
+		self->y += self->dirY;
+	}
+}
+
+static void pieceFallout()
+{
+	self->head->health = -1;
+	
+	self->inUse = FALSE;
+}
+
+static void redTakeDamage(Entity *other, int damage)
+{
+	if (self->flags & INVULNERABLE)
+	{
+		return;
+	}
+
+	if (damage != 0)
+	{
+		self->health -= damage;
+
+		if (other->type == PROJECTILE)
+		{
+			other->target = self;
+		}
+
+		if (self->health > 0)
+		{
+			setCustomAction(self, &flashWhite, 6, 0, 0);
+
+			/* Don't make an enemy invulnerable from a projectile hit, allows multiple hits */
+
+			if (other->type != PROJECTILE)
+			{
+				setCustomAction(self, &invulnerableNoFlash, 20, 0, 0);
+			}
+
+			if (self->pain != NULL)
+			{
+				self->pain();
+			}
+		}
+
+		else
+		{
+			self->animationCallback = NULL;
+			
+			self->damage = 0;
+
+			self->die();
+		}
+	}
+}
---
+++ tags/0.67/src/medal.c
@@ -0,0 +1,385 @@
+/*
+Copyright (C) 2009-2010 Parallel Realities
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include "headers.h"
+
+#include "system/load_save.h"
+#include "system/error.h"
+#include "hud.h"
+#include "medal.h"
+#include "graphics/font.h"
+#include "graphics/graphics.h"
+#include "input.h"
+
+extern Game game;
+
+static Medal medal;
+static Message messageHead;
+
+static void addMedalToQueue(char *);
+static int postMedal(void *);
+static void getNextMedalFromQueue(void);
+
+void initMedals()
+{
+	messageHead.next = NULL;
+
+	medal.medalMessage.text[0] = '\0';
+
+	connectToServer();
+}
+
+void addMedal(char *medalName)
+{
+	if (medal.connected == TRUE && medal.privateKeyFound == TRUE)
+	{
+		addMedalToQueue(medalName);
+	}
+}
+
+void processMedals()
+{
+	if (medal.processing == FALSE)
+	{
+		if (strlen(medal.medalMessage.text) == 0)
+		{
+			getNextMedalFromQueue();
+		}
+
+		else
+		{
+			medal.processing = TRUE;
+
+			medal.thread = SDL_CreateThread(&postMedal, NULL);
+		}
+	}
+}
+
+static void addMedalToQueue(char *text)
+{
+	Message *head, *msg;
+
+	head = &messageHead;
+
+	while (head->next != NULL)
+	{
+		if (strcmpignorecase(text, head->text) == 0)
+		{
+			return;
+		}
+
+		head = head->next;
+	}
+
+	msg = (Message *)malloc(sizeof(Message));
+
+	if (msg == NULL)
+	{
+		showErrorAndExit("Failed to allocate %d bytes for Medal queue", (int)sizeof(Message));
+	}
+
+	STRNCPY(msg->text, text, sizeof(messageHead.text));
+
+	msg->next = NULL;
+
+	head->next = msg;
+}
+
+static void getNextMedalFromQueue()
+{
+	Message *head = messageHead.next;
+
+	if (head != NULL)
+	{
+		STRNCPY(medal.medalMessage.text, head->text, sizeof(medal.medalMessage.text));
+
+		messageHead.next = head->next;
+
+		free(head);
+	}
+}
+
+void freeMedalQueue()
+{
+	Message *p, *q;
+
+	for (p=messageHead.next;p!=NULL;p=q)
+	{
+		q = p->next;
+
+		free(p);
+	}
+
+	messageHead.next = NULL;
+
+	medal.medalMessage.text[0] = '\0';
+
+	SDL_KillThread(medal.thread);
+}
+
+static int postMedal(void *data)
+{
+	char medalName[MAX_MESSAGE_LENGTH], in[MAX_LINE_LENGTH], out[2][MAX_LINE_LENGTH];
+	char *token, *store;
+	int i, len, response[2];
+	TCPsocket socket;
+
+	STRNCPY(medalName, medal.medalMessage.text, MAX_MESSAGE_LENGTH);
+
+	for (i=0;i<strlen(medalName);i++)
+	{
+		if (medalName[i] == ' ' || medalName[i] == '#' || medalName[i] == ',')
+		{
+			medalName[i] = '_';
+		}
+	}
+
+	medal.medalMessage.text[0] = '\0';
+
+	socket = SDLNet_TCP_Open(&medal.ip);
+
+	if (socket == NULL)
+	{
+		printf("Failed to create socket: %s\n", SDLNet_GetError());
+
+		medal.processing = FALSE;
+
+		return 0;
+	}
+
+	snprintf(out[0], MAX_LINE_LENGTH, "GET /addMedal/%s/LOE_%s HTTP/1.1\nHost: %s\nUser-Agent:LOE%.2f-%d\n\n", medal.privateKey, medalName, MEDAL_SERVER_HOST, VERSION, RELEASE);
+
+	len = strlen(out[0]) + 1;
+
+	if (SDLNet_TCP_Send(socket, (void*)out[0], len) < len)
+	{
+		printf("Medal sending failed: %s\n", SDLNet_GetError());
+
+		printf("Disconnected\n");
+
+		SDLNet_TCP_Close(socket);
+
+		medal.processing = FALSE;
+
+		return 0;
+	}
+
+	SDLNet_TCP_Recv(socket, in, MAX_LINE_LENGTH - 1);
+
+	response[0] = response[1] = 0;
+
+	token = strtok_r(in, "\n", &store);
+
+	i = 0;
+
+	while (token != NULL)
+	{
+		if (strstr(token, "MEDAL_RESPONSE"))
+		{
+			sscanf(token, "%*s %d %[^\n\r]", &response[i], out[i]);
+
+			i++;
+
+			if (i == 2)
+			{
+				break;
+			}
+		}
+
+		token = strtok_r(NULL, "\n", &store);
+	}
+
+	SDLNet_TCP_Close(socket);
+
+	printf("MedalServer Response: %d '%s'\n", response[1], out[1]);
+
+	if (response[1] > 0 && response[1] <= 4)
+	{
+		while (showMedal(response[1], out[1]) == FALSE)
+		{
+			SDL_Delay(1000);
+		}
+	}
+
+	printf("MedalServer Response: %d '%s'\n", response[0], out[0]);
+
+	if (response[0] > 0 && response[0] <= 4)
+	{
+		while (showMedal(response[0], out[0]) == FALSE)
+		{
+			SDL_Delay(1000);
+		}
+	}
+	
+	medal.processing = FALSE;
+
+	return 1;
+}
+
+int connectToServer()
+{
+	if (medal.connected == TRUE)
+	{
+		return 0;
+	}
+
+	if (getPrivateKey(medal.privateKey) == FALSE)
+	{
+		medal.privateKeyFound = FALSE;
+
+		return 1;
+	}
+
+	medal.privateKeyFound = TRUE;
+
+	printf("Trying to connect to %s:%d\n", MEDAL_SERVER_HOST, MEDAL_SERVER_PORT);
+
+	if (SDLNet_ResolveHost(&medal.ip, MEDAL_SERVER_HOST, MEDAL_SERVER_PORT) == -1)
+	{
+		printf("Could not connect to medal server: %s\n", SDLNet_GetError());
+
+		return 2;
+	}
+
+	printf("Connected %s to %s:%d\n", medal.privateKey, MEDAL_SERVER_HOST, MEDAL_SERVER_PORT);
+
+	medal.connected = TRUE;
+
+	return 0;
+}
+
+void medalProcessingFinished()
+{
+	medal.processing = FALSE;
+}
+
+void showMedalScreen()
+{
+	int i, h, y;
+	SDL_Rect src, dest;
+	SDL_Surface *title, *message, *message2;
+	char text[MAX_MESSAGE_LENGTH];
+
+	if (medal.privateKeyFound == TRUE)
+	{
+		if (medal.connected == TRUE)
+		{
+			return;
+		}
+
+		else
+		{
+			snprintf(text, MAX_MESSAGE_LENGTH, _("Could not connect to Medal Server at %s:%d"), MEDAL_SERVER_HOST, MEDAL_SERVER_PORT);
+
+			message = generateTextSurface(text, game.font, 220, 0, 0, 0, 0, 0);
+		}
+	}
+
+	else
+	{
+		snprintf(text, MAX_MESSAGE_LENGTH, _("Could not find Medal Key"));
+
+		message = generateTextSurface(text, game.font, 220, 0, 0, 0, 0, 0);
+	}
+
+	title = generateTextSurface(_("The Legend of Edgar"), game.font, 0, 220, 0, 0, 0, 0);
+
+	message2 = generateTextSurface(_("You will not be able to earn Medals for this game"), game.font, 220, 0, 0, 0, 0, 0);
+
+	if (title == NULL || message == NULL || message2 == NULL)
+	{
+		return;
+	}
+
+	if (game.tempSurface != NULL)
+	{
+		SDL_FreeSurface(game.tempSurface);
+
+		game.tempSurface = NULL;
+	}
+
+	game.tempSurface = createSurface(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+	drawBox(game.tempSurface, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0);
+
+	h = title->h + message->h + message2->h + 90;
+
+	y = (SCREEN_HEIGHT - h) / 2;
+
+	src.x = 0;
+	src.y = 0;
+	src.w = title->w;
+	src.h = title->h;
+
+	dest.x = (SCREEN_WIDTH - title->w) / 2;
+	dest.y = y;
+	dest.w = title->w;
+	dest.h = title->h;
+
+	SDL_BlitSurface(title, &src, game.tempSurface, &dest);
+
+	y += message->h + 45;
+
+	src.x = 0;
+	src.y = 0;
+	src.w = message->w;
+	src.h = message->h;
+
+	dest.x = (SCREEN_WIDTH - message->w) / 2;
+	dest.y = y;
+	dest.w = message->w;
+	dest.h = message->h;
+
+	SDL_BlitSurface(message, &src, game.tempSurface, &dest);
+
+	y += message2->h + 45;
+
+	src.x = 0;
+	src.y = 0;
+	src.w = message2->w;
+	src.h = message2->h;
+
+	dest.x = (SCREEN_WIDTH - message2->w) / 2;
+	dest.y = y;
+	dest.w = message2->w;
+	dest.h = message2->h;
+
+	SDL_BlitSurface(message2, &src, game.tempSurface, &dest);
+
+	SDL_FreeSurface(title);
+	SDL_FreeSurface(message);
+	SDL_FreeSurface(message2);
+
+	for (i=0;i<120;i++)
+	{
+		getInput(IN_TITLE);
+
+		clearScreen(0, 0, 0);
+
+		SDL_BlitSurface(game.tempSurface, NULL, game.screen, NULL);
+
+		/* Swap the buffers */
+
+		SDL_Flip(game.screen);
+
+		/* Sleep briefly */
+
+		SDL_Delay(16);
+	}
+}
