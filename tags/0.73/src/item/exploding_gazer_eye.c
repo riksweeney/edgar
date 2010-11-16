5999ebbfe755bf54b3a58b5b2d782d821b03c935
nobody <nobody@b0f73b58-f77c-4d9d-b241-b801c8a9558b>
2010-11-16 23:23:13 +0000

    Tagging 0.73

---
+++ tags/0.73/src/item/exploding_gazer_eye.c
@@ -0,0 +1,115 @@
+/*
+Copyright (C) 2009-2010 Parallel Realities
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include "../headers.h"
+
+#include "../graphics/animation.h"
+#include "../audio/audio.h"
+#include "../system/properties.h"
+#include "../entity.h"
+#include "../collisions.h"
+#include "../system/error.h"
+#include "../player.h"
+#include "../game.h"
+
+extern Entity *self;
+
+static void entityWait(void);
+static void explodeInit(void);
+static void explode(void);
+
+Entity *addExplodingGazerEye(int x, int y, char *name)
+{
+	Entity *e = getFreeEntity();
+
+	if (e == NULL)
+	{
+		showErrorAndExit("No free slots to add an Exploding Gazer Eye");
+	}
+
+	loadProperties(name, e);
+
+	e->x = x;
+	e->y = y;
+
+	e->type = KEY_ITEM;
+
+	e->action = &entityWait;
+	e->touch = &entityTouch;
+	e->takeDamage = &entityTakeDamageNoFlinch;
+	e->die = &entityDieNoDrop;
+	e->pain = &enemyPain;
+
+	e->draw = &drawLoopingAnimationToMap;
+
+	setEntityAnimation(e, STAND);
+
+	return e;
+}
+
+static void entityWait()
+{
+	checkToMap(self);
+	
+	if (self->flags & ON_GROUND)
+	{
+		self->dirX = 0;
+		
+		self->thinkTime--;
+		
+		if (self->thinkTime <= 0)
+		{
+			setEntityAnimation(self, WALK);
+			
+			self->thinkTime = 15;
+			
+			self->action = &explodeInit;
+		}
+	}
+}
+
+static void explodeInit()
+{
+	self->thinkTime--;
+	
+	if (self->thinkTime <= 0)
+	{
+		self->thinkTime = 15;
+		
+		setEntityAnimation(self, JUMP);
+		
+		self->action = &explode;
+	}
+}
+
+static void explode()
+{
+	self->thinkTime--;
+	
+	if (self->thinkTime <= 0)
+	{
+		playSoundToMap("sound/enemy/gazer/flash.ogg", -1, self->x, self->y, 0);
+
+		fadeFromColour(255, 255, 255, 60);
+		
+		self->inUse = FALSE;
+		
+		setPlayerStunned(300);
+	}
+}
---
+++ tags/0.73/src/item/spore_crop.c
@@ -0,0 +1,83 @@
+/*
+Copyright (C) 2009-2010 Parallel Realities
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include "../headers.h"
+
+#include "../graphics/animation.h"
+#include "../audio/audio.h"
+#include "../system/properties.h"
+#include "../entity.h"
+#include "key_items.h"
+#include "item.h"
+#include "spore.h"
+#include "../inventory.h"
+#include "../system/error.h"
+
+extern Entity *self, player;
+
+static void collectSpore(Entity *);
+
+Entity *addSporeCrop(int x, int y, char *name)
+{
+	Entity *e = getFreeEntity();
+
+	if (e == NULL)
+	{
+		showErrorAndExit("No free slots to add a Spore Crop");
+	}
+
+	loadProperties(name, e);
+
+	e->x = x;
+	e->y = y;
+
+	e->type = KEY_ITEM;
+
+	e->face = RIGHT;
+
+	e->action = &doNothing;
+	e->touch = &collectSpore;
+
+	e->draw = &drawLoopingAnimationToMap;
+
+	setEntityAnimation(e, STAND);
+
+	e->thinkTime = 0;
+
+	return e;
+}
+
+static void collectSpore(Entity *other)
+{
+	Entity *spore = NULL;
+
+	if (self->thinkTime <= 0)
+	{
+		if (getInventoryItemByObjectiveName("Spore") == NULL)
+		{
+			spore = addSpore(other->x, other->y, "item/spores");
+
+			STRNCPY(spore->objectiveName, "Spore", sizeof(spore->objectiveName));
+
+			addToInventory(spore);
+		}
+
+		self->thinkTime = self->maxThinkTime;
+	}
+}
