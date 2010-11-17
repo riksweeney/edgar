94e5e995802aad4c1d823da1d5feea95c4be22e8
nobody <nobody@b0f73b58-f77c-4d9d-b241-b801c8a9558b>
2010-11-17 08:07:04 +0000

    Tagging 0.73

---
+++ tags/0.73/src/graphics/decoration.h
@@ -0,0 +1,36 @@
+/*
+Copyright (C) 2009-2010 Parallel Realities
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+void freeDecorations(void);
+Entity *getFreeDecoration(void);
+void doDecorations(void);
+void drawDecorations(void);
+void addStarExplosion(int, int);
+void addSparkle(int, int);
+Entity *addTrail(int, int, char *, int);
+Entity *addDecoration(char *, int, int);
+Entity *addSmoke(int, int, char *);
+Entity *addMultipleSparkles(int, int, char *);
+void addDecorationFromScript(char *);
+void addBlood(int, int);
+Entity *addBasicDecoration(int, int, char *);
+void addParticleExplosion(int, int);
+Entity *addPixelDecoration(int, int);
+Entity *addParticle(int, int);
+Entity *addDamageScore(int, Entity *);
