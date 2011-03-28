847f4a144a847bfca8bdaba7dd9e9840de661ba5
nobody <nobody@b0f73b58-f77c-4d9d-b241-b801c8a9558b>
2011-03-28 18:02:44 +0000

    Tagging 0.81

---
+++ tags/0.81/src/item/snow_pile.c
@@ -0,0 +1,93 @@
+/*
+Copyright (C) 2009-2011 Parallel Realities
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include "../headers.h"
+
+#include "../graphics/animation.h"
+#include "../audio/audio.h"
+#include "../system/properties.h"
+#include "../entity.h"
+#include "../collisions.h"
+#include "item.h"
+#include "../system/error.h"
+#include "../system/random.h"
+
+extern Entity *self, player;
+
+static void entityWait(void);
+static void die(void);
+
+Entity *addSnowPile(int x, int y, char *name)
+{
+	Entity *e = getFreeEntity();
+
+	if (e == NULL)
+	{
+		showErrorAndExit("No free slots to add a Snow Pile");
+	}
+
+	loadProperties(name, e);
+
+	e->x = x;
+	e->y = y;
+
+	e->type = KEY_ITEM;
+
+	e->action = &entityWait;
+	e->die = &die;
+
+	e->draw = &drawLoopingAnimationToMap;
+
+	setEntityAnimation(e, "STAND");
+
+	return e;
+}
+
+static void entityWait()
+{
+	#if DEV == 0
+	self->face = player.face;
+
+	self->x = player.x + player.w / 2 - self->w / 2;
+	self->y = player.y;
+	#endif
+}
+
+static void die()
+{
+	int i;
+	Entity *e;
+
+	self->targetX = playSoundToMap("sound/enemy/giant_snowball/crumble.ogg", -1, self->x, self->y, 0);
+
+	for (i=0;i<32;i++)
+	{
+		e = addTemporaryItem("misc/giant_snowball_piece", self->x, self->y, RIGHT, 0, 0);
+
+		e->x += prand() % self->w;
+		e->y += prand() % self->h;
+
+		e->dirX = (prand() % 4) * (prand() % 2 == 0 ? -1 : 1);
+		e->dirY = -2.5;
+
+		e->thinkTime = 60 + (prand() % 120);
+	}
+
+	self->inUse = FALSE;
+}
