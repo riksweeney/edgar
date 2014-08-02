306f3ff8270701f8d766443ef6b0daf0976be683
riksweeney <riksweeney@b0f73b58-f77c-4d9d-b241-b801c8a9558b>
2014-08-02 08:16:05 +0000

    Tagging 1.17

---
+++ tags/1.17/edgar.spec
@@ -0,0 +1,40 @@
+Summary: 2D Platform Game
+Name: %{name}
+Version: %{version}
+Release: %{release}
+Source: %{name}-%{version}-%{release}.tar.gz
+Vendor: Parallel Realities
+Packager: Richard Sweeney
+URL: http://www.parallelrealities.co.uk/p/legend-of-edgar.html
+Group: Games/Arcade
+License: GPL
+%description
+The Legend of Edgar. When his father fails to return home after venturing
+out one stormy night, Edgar sets off on a quest to rescue him.
+
+%prep
+%setup -q
+
+%build
+make VERSION=%{version} RELEASE=%{release}
+
+%install
+make install DESTDIR=$RPM_BUILD_ROOT
+
+%clean
+
+%post
+
+
+%files
+%dir /usr/games
+%dir /usr/share/games/edgar
+%dir /usr/share/doc/%{name}
+/usr/games/%{name}
+/usr/share/games/edgar/%{name}.pak
+/usr/share/doc/%{name}/*
+/usr/share/applications/edgar.desktop
+/usr/share/icons/hicolor/16x16/apps/edgar.png
+/usr/share/icons/hicolor/32x32/apps/edgar.png
+/usr/share/icons/hicolor/48x48/apps/edgar.png
+/usr/share/icons/hicolor/64x64/apps/edgar.png
---
+++ tags/1.17/src/enemy/grub.c
@@ -0,0 +1,142 @@
+/*
+Copyright (C) 2009-2014 Parallel Realities
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110-1335, USA.
+*/
+
+#include "../headers.h"
+
+#include "../audio/audio.h"
+#include "../collisions.h"
+#include "../entity.h"
+#include "../graphics/animation.h"
+#include "../system/error.h"
+#include "../system/properties.h"
+
+extern Entity *self;
+
+static void move(void);
+static void moveReckless(void);
+static void creditsMove(void);
+static void die(void);
+
+Entity *addGrub(int x, int y, char *name)
+{
+	Entity *e = getFreeEntity();
+
+	if (e == NULL)
+	{
+		showErrorAndExit("No free slots to add a Grub");
+	}
+
+	loadProperties(name, e);
+
+	e->x = x;
+	e->y = y;
+
+	e->action = &move;
+
+	e->draw = &drawLoopingAnimationToMap;
+	e->touch = &entityTouch;
+	e->die = &die;
+	e->takeDamage = &entityTakeDamageFlinch;
+	e->reactToBlock = &changeDirection;
+
+	e->creditsAction = &creditsMove;
+
+	e->type = ENEMY;
+
+	setEntityAnimation(e, "STAND");
+
+	return e;
+}
+
+static void die()
+{
+	playSoundToMap("sound/enemy/grub/grub_die", -1, self->x, self->y, 0);
+
+	entityDie();
+}
+
+static void move()
+{
+	moveLeftToRight();
+	
+	if (onSingleTile(self) == TRUE)
+	{
+		self->action = &moveReckless;
+	}
+}
+
+static void moveReckless()
+{
+	long onGround = self->flags & ON_GROUND;
+	
+	if (self->dirX == 0)
+	{
+		self->x += self->face == LEFT ? self->box.x : -self->box.x;
+
+		self->face = self->face == RIGHT ? LEFT : RIGHT;
+	}
+
+	if (self->standingOn == NULL || self->standingOn->dirX == 0)
+	{
+		self->dirX = (self->face == RIGHT ? self->speed : -self->speed);
+	}
+
+	else
+	{
+		self->dirX += (self->face == RIGHT ? self->speed : -self->speed);
+	}
+
+	checkToMap(self);
+
+	if (self->dirX == 0)
+	{
+		self->dirX = (self->face == RIGHT ? -self->speed : self->speed);
+
+		self->face = (self->face == RIGHT ? LEFT : RIGHT);
+	}
+	
+	if ((self->flags & ON_GROUND) || self->standingOn != NULL)
+	{
+		if (landedOnGround(onGround) == TRUE)
+		{
+			self->action = &move;
+		}
+	}
+}
+
+static void creditsMove()
+{
+	self->thinkTime++;
+
+	if (self->thinkTime > 400)
+	{
+		self->mental = 1;
+	}
+
+	setEntityAnimation(self, "STAND");
+
+	self->dirX = self->speed;
+
+	checkToMap(self);
+
+	if (self->dirX == 0)
+	{
+		self->inUse = FALSE;
+	}
+}
