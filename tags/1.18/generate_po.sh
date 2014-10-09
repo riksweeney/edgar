2a736e33cac6155aaf6b02460ae08aa46a5e0ea8
riksweeney <riksweeney@b0f73b58-f77c-4d9d-b241-b801c8a9558b>
2014-10-09 20:55:32 +0000

    Tagging 1.18

---
+++ tags/1.18/generate_po.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+FILES=`find . -name *.c`
+
+xgettext --no-wrap --keyword='_' $FILES
+
+msgcat --no-wrap locale/edgar.pot > t
+mv t locale/edgar.pot
+head -n 15 locale/edgar.pot > t
+./po_creator >> t
+mv t locale/edgar.pot
