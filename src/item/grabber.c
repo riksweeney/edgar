c4998bdc5151c5a61a38d5d468cc019e5350ae59
riksweeney <riksweeney@users.noreply.github.com>
2018-01-01 08:47:07 +0000

    Updated copyright year

--- src/item/grabber.c
+++ src/item/grabber.c
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2009-2017 Parallel Realities
+Copyright (C) 2009-2018 Parallel Realities
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
